46786ff3aa8a51195f65b393389ae7654a23ced0
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-13 23:33:14 +0500

    created api using express to read data using the get http method

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'hello from the server side',
+//     app: 'natours',
+//   });
+// });
+
+//reading file at the top level and pasre into object using the JSON.parase function
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+//create api to read the file
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'hello from the server side',
-    app: 'natours',
+    status: 'success',
+    //if we send multiple responses than we have to use this result
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
