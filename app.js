eb96bd0f99fceb6f539e99b5936200ece6cdb72a
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-13 15:19:46 +0500

    app.js file created performed simple get and post request using express

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'hello from the server side',
+    app: 'natours',
+  });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app runing on port ${port}`);
+});
