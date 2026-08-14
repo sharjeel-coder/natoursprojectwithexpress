e762dcce4f1ed9c3b67b28dc7066ba382772bb5d
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-14 18:01:36 +0500

    created post request and write data back to tour-sample.json

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({
@@ -25,8 +26,26 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint');
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  // new tour object created
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  //writing into the file using the fs module
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 const port = 3000;
