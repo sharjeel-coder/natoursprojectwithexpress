e79f653a87c8f25a8268943626907839dba5c638
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-15 02:15:32 +0500

    implemented patch method to update the json

--- app.js
+++ app.js
@@ -68,6 +68,22 @@ app.post('/api/v1/tours', (req, res) => {
     },
   );
 });
+//to update the the tour using the patch method
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<UPDATED TOUR HERE>',
+    },
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
