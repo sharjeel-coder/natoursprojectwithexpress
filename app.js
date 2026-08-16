e61f221a4bf0bc1ff17e4633b6aaa351c53f2ac7
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-16 13:22:46 +0500

    mounted users and tour routes

--- app.js
+++ app.js
@@ -162,26 +162,23 @@ const deleteUser = (req, res) => {
 // //delete method
 
 // app.delete('/api/v1/tours/:id', deleteTour);
-
 // instead of doing all this above we can do it like this
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 //for users
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+//mounting routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = 3000;
 app.listen(port, () => {
