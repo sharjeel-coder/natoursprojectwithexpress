01fdff1f9bb00528b63fc57bb46107b677e78c17
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-17 00:45:38 +0500

    routes and controller folder created and change file sturuture

---
+++ controllers/tourController.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const fs = require('fs');
+
+//reading file at the top level and pasre into object using the JSON.parase function
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+);
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    // if we send multiple responses than we have to use this result
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+// GET one tour
+exports.getTour = (req, res) => {
+  // console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+// POST tour
+exports.createTour = (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+
+  // new tour object created
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  // writing into the file using the fs module
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+};
+
+// PATCH tour
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<UPDATED TOUR HERE>',
+    },
+  });
+};
+
+// DELETE tour
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
