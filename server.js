3a2dae32db69d33c52ab9dd804e45c04baffc7c4
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-17 22:45:15 +0500

    setting up the development enviroment

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+console.log(process.env);
 
 const port = 3000;
 app.listen(port, () => {
