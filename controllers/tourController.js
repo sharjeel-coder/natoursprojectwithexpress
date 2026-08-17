5b6ae0acbb2bb1458dc577aca359d3b2a4dfffe4
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-17 16:11:47 +0500

    implemented the params middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,6 +6,18 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
+// param middleware function
+exports.checkID = (req, res, next, val) => {
+  console.log(`the value of tour id is :${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -24,13 +36,6 @@ exports.getTour = (req, res) => {
   // console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -66,13 +71,6 @@ exports.createTour = (req, res) => {
 
 // PATCH tour
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -83,13 +81,6 @@ exports.updateTour = (req, res) => {
 
 // DELETE tour
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,11 @@ const tourController = require('./../controllers/tourController');
 const fs = require('fs');
 
 const router = express.Router();
+//params middleware
+// router.param('id', (req, res, next, val) => {
+//   next();
+// });instead of above this all we can do this as below we created middleware parmams funtion in the tourcontroller and exported it.
+router.param('id', tourController.checkID);
 
 router
   .route('/')
