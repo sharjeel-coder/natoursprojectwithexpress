e04e1dfd4c1da45ffbcd4fd1520bb225a3eeaab0
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-17 17:21:06 +0500

    created middleware to check request body and chain it with createtour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -17,7 +17,16 @@ exports.checkID = (req, res, next, val) => {
   }
   next();
 };
-
+// quiz checkreqbody
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'missing name or price',
+    });
+  }
+  next();
+};
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
