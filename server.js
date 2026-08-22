5e4f0d60ebd2212211ef90b784b00b8e12c411e5
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-23 00:42:45 +0500

    created model and saved to local database

--- server.js
+++ server.js
@@ -54,6 +54,19 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+// creating new tours
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR', err);
+  });
 const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`app runing on port ${port}`);
